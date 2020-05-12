b1d79fad322062085741e460d19479c96bb2c8df
“Lauren <lucerolaurenm@gmail.com>
2020-05-12 16:31:02 -0400

    Add fetch to get area api data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,7 +11,8 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      user: ''
+      user: '',
+      areas: []
     }
   }
 
@@ -19,7 +20,47 @@ class App extends React.Component {
     this.setState({user: newUser})
     console.log(this.state.user)
   }
-  
+
+  componentDidMount() {
+    fetch('https://vrad-api.herokuapp.com/api/v1/areas')
+      .then(response => response.json())
+      .then(areaData => {
+        const promises = areaData.areas.map(area => {
+          let url = `https://vrad-api.herokuapp.com${area.details}`
+          return fetch(url)
+          .then(response => response.json())
+          .then(areaDetails => areaDetails)
+        
+      })
+      return Promise.all(promises)
+    })
+    .then(resolvedData => this.setState({areas: resolvedData}))
+    .catch(err => console.error(err))
+}
+
+componentDidUpdate() {
+  console.log(this.state.areas)
+}
+
+//   handleApiData = (data) => {
+//     const promises = data.areas.map(area => {
+//       let url = `https://vrad-api.herokuapp.com${area.details}`
+//       fetch(url)
+//       .then(response => response.json())
+//       .then(data => data)
+//       .catch(err => console.error(err))  
+//       return   
+//     })
+// ``
+//   }
+
+  fetchAreaDetails = (url) => {
+    // fetch(url)
+    // .then(response => response.json())
+    // .then(data => console.log(data))
+    // .catch(err => console.error(err))
+  }
+
   render() {
     return (
       <div className="App">
