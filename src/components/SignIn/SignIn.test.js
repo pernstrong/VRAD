2e3f0437c7f6b10ea01cff2dbb55dea8bf6ad966
“Lauren <lucerolaurenm@gmail.com>
2020-05-15 08:30:08 -0400

    Finish SignIn testing

--- src/components/SignIn/SignIn.test.js
+++ src/components/SignIn/SignIn.test.js
@@ -1,23 +1,63 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 import { render, fireEvent } from "@testing-library/react";
 import SignIn from "./SignIn";
+import "@testing-library/jest-dom/extend-expect";
 
 describe("SignIn", () => {
-  it("displays a welcome message", () => {
-    const { getByText } = render(
-      <BrowserRouter>
+  it("should render sign in form", () => {
+    const { getByText, getByLabelText } = render(
+      <MemoryRouter>
         <SignIn />
-      </BrowserRouter>
+      </MemoryRouter>
     );
     expect(getByText("Welcome, please sign in!")).toBeInTheDocument();
+    expect(getByLabelText("Enter your name:")).toBeInTheDocument();
+    expect(getByLabelText("Enter your email:")).toBeInTheDocument();
+    expect(getByLabelText("Why are you visiting?")).toBeInTheDocument();
+  });
+
+  it("should render error message if missing input", () => {
+    const { getByText, getByLabelText, getByPlaceholderText } = render(
+      <MemoryRouter>
+        <SignIn />
+      </MemoryRouter>
+    );
+    fireEvent.change(getByPlaceholderText("Your name"), {
+      target: { value: "" },
+    });
+    fireEvent.change(getByPlaceholderText("Your email"), {
+      target: { value: "" },
+    });
+    fireEvent.change(getByLabelText("Why are you visiting?"), {
+      target: { value: "Business" },
+    });
+    fireEvent.click(getByText("Sign In"));
+    expect(getByText("Please fill in all inputs.")).toBeInTheDocument();
+  });
+
+  it("should run setUser if all inputs are filled in", () => {
+    const mockSetUser = jest.fn();
+    const { getByText, getByPlaceholderText, getByLabelText } = render(
+      <MemoryRouter>
+        <SignIn setUser={mockSetUser} />
+      </MemoryRouter>
+    );
+    fireEvent.change(getByPlaceholderText("Your name"), {
+      target: { value: "Foxy Meatball" },
+    });
+    fireEvent.change(getByPlaceholderText("Your email"), {
+      target: { value: "foxymeatball@aol.com" },
+    });
+    fireEvent.change(getByLabelText("Why are you visiting?"), {
+      target: { value: "vacation" },
+    });
+    fireEvent.click(getByText("Sign In"));
+    expect(mockSetUser).toHaveBeenCalledTimes(1);
+    expect(mockSetUser).toHaveBeenCalledWith({
+      name: "Foxy Meatball",
+      email: "foxymeatball@aol.com",
+      purpose: "vacation",
+    });
   });
 });
-
-/* Lauren thinking...
-Passed one test. Wondering if additional display tests should live in this it block / if these display tests are necessary. 
-Next tests that I would add to this file would be fireEvent tests that check handleChange and handleClick functionality
-*/
-
-module3/vrad/public/images/3_a.jpg
-/Users/laurenlucero/Turing/module3/vrad/public/images/3_a.jpg
