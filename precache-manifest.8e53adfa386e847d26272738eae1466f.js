bd384684c999730c5738da8b3870fe717534398e
Dave Pernitz <pernitz@uwalumni.com>
2020-05-21 14:37:20 -0600

    Updates

---
+++ precache-manifest.8e53adfa386e847d26272738eae1466f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352a430e31be04008a6fda355bb45905",
+    "url": "/VRAD/index.html"
+  },
+  {
+    "revision": "740fa36b1584e990cd3f",
+    "url": "/VRAD/static/css/main.91469d88.chunk.css"
+  },
+  {
+    "revision": "71d2316ede3d31e166a2",
+    "url": "/VRAD/static/js/2.714f3d08.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/VRAD/static/js/2.714f3d08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "740fa36b1584e990cd3f",
+    "url": "/VRAD/static/js/main.ccfe9dfa.chunk.js"
+  },
+  {
+    "revision": "7a2dddbd42d3d7fce8d1",
+    "url": "/VRAD/static/js/runtime-main.46ed73d9.js"
+  }
+]);
